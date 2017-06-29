d7f4897d347016142478ef047bff75c23847b710
zhouyu <2530510290@qq.com>
2017-06-29 23:32:12 +0800

    add

--- src/test/java/com/nextyu/demo/test/MyTest.java
+++ src/test/java/com/nextyu/demo/test/MyTest.java
@@ -3,6 +3,10 @@ package com.nextyu.demo.test;
 import com.nextyu.summer.framework.util.ClassUtil;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * created on 2017-06-23 14:20
  *
@@ -14,5 +18,126 @@ public class MyTest {
     public void test() {
         Class<?> aClass = ClassUtil.loadClass("com.nextyu.summer.framework.helper.ClassHelper");
         System.out.println(aClass);
+    }
+
+    @Test
+    public void test2() {
+        int quantity = 6;
+        List<Person> people = new ArrayList<>(quantity);
+        for (long i = 0; i < quantity; i++) {
+            Person person = new Person(i, false);
+            people.add(person);
+        }
+
+        test23(people, quantity);
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+
+    }
+
+    private void test24(List<Person> people, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Person person = people.get(i);
+            int rightChance = quantity - 3;
+            if (null != person.getRight() || null != person.getLeft()) {
+                rightChance -= 1;
+            }
+
+        }
+    }
+
+
+    private void test23(List<Person> people, int quantity) {
+        int jump = 2;
+        for (int i = 0; i < quantity; i++) {
+            Person p = people.get(i);
+            int left;
+            int right;
+
+            if (i == 0) {
+                left = quantity - 1;
+                right = +1;
+            } else if (i == quantity - 1) {
+                left = i - 1;
+                right = 0;
+            } else {
+                left = i - 1;
+                right = i + 1;
+            }
+
+
+            for (Person person : people) {
+                Long id = person.getId();
+                if (p.getId().longValue() != id && left != id
+                        && right != id) {
+
+                    if (null == p.getLeft() || null == person.getRight()) {
+                        if (null == p.getRight() && null == person.getLeft()) {
+                            p.setRight(person);
+                            person.setLeft(p);
+                            break;
+                        }
+                    } else {
+                        if (p.getLeft().getId() != person.getId()) {
+                            p.setRight(person);
+                            person.setLeft(p);
+                            break;
+                        }
+                    }
+
+
+                }
+            }
+
+
+//            System.out.println(i + "--" + left + "--" + right);
+            // System.out.println(p);
+
+        }
+    }
+
+    private void test22(List<Person> people, int quantity) {
+        int jump = 2;
+        for (int i = 0; i < quantity; i++) {
+
+
+            int left = i - jump;
+            if (left < 0) {
+                left += quantity;
+            }
+
+            int right = i + jump;
+            if (right >= quantity) {
+                right -= quantity;
+            }
+
+            System.out.println(i + "--" + left + "--" + right);
+
+        }
+    }
+
+    private void test21(List<Person> people, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Person person = people.get(i);
+            Person left;
+            Person right;
+            if (i == 0) {
+                left = people.get(quantity - 1);
+                right = people.get(i + 1);
+            } else if (i == quantity - 1) {
+                left = people.get(i - 1);
+                right = people.get(0);
+            } else {
+                left = people.get(i - 1);
+                right = people.get(i + 1);
+            }
+
+            person.setLeft(left);
+            person.setRight(right);
+
+            System.out.println(person);
+        }
     }
 }
--- src/test/java/com/nextyu/demo/test/Person.java
+++ src/test/java/com/nextyu/demo/test/Person.java
@@ -1,5 +1,7 @@
 package com.nextyu.demo.test;
 
+import lombok.Data;
+
 /**
  * created on 2017-06-29 18:11
  *
@@ -11,4 +13,54 @@ public class Person {
 
     private Person left;
     private Person right;
+
+    public Person() {
+    }
+
+    public Person(Long id, Boolean isOut) {
+        this.id = id;
+        this.isOut = isOut;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getIsOut() {
+        return isOut;
+    }
+
+    public void setIsOut(Boolean isOut) {
+        this.isOut = isOut;
+    }
+
+    public Person getLeft() {
+        return left;
+    }
+
+    public void setLeft(Person left) {
+        this.left = left;
+    }
+
+    public Person getRight() {
+        return right;
+    }
+
+    public void setRight(Person right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", isOut=" + isOut +
+                ", left=" + left.getId() +
+                ", right=" + right.getId() +
+                '}';
+    }
 }
