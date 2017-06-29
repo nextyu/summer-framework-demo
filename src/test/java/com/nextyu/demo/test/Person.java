00f386889af0e9f696b2a4e16bb756fc014a918d
zhouyu <1467237221@qq.com>
2017-06-29 18:20:50 +0800

    modify web.xml

---
+++ src/test/java/com/nextyu/demo/test/Person.java
@@ -0,0 +1,14 @@
+package com.nextyu.demo.test;
+
+/**
+ * created on 2017-06-29 18:11
+ *
+ * @author nextyu
+ */
+public class Person {
+    public Long id;
+    private Boolean isOut;
+
+    private Person left;
+    private Person right;
+}
