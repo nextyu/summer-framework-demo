342e81f6ef32edbf8ac7179d5472ca147c09e2ed
zhouyu <2530510290@qq.com>
2017-07-02 17:19:29 +0800

    add

--- src/test/java/com/nextyu/demo/test/Person.java
+++ src/test/java/com/nextyu/demo/test/Person.java
@@ -1,12 +1,17 @@
 package com.nextyu.demo.test;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * created on 2017-06-29 18:11
  *
  * @author nextyu
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Person {
     public Long id;
     private Boolean isOut;
@@ -14,45 +19,13 @@ public class Person {
     private Person left;
     private Person right;
 
-    public Person() {
-    }
 
     public Person(Long id, Boolean isOut) {
         this.id = id;
         this.isOut = isOut;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Boolean getIsOut() {
-        return isOut;
-    }
-
-    public void setIsOut(Boolean isOut) {
-        this.isOut = isOut;
-    }
-
-    public Person getLeft() {
-        return left;
-    }
-
-    public void setLeft(Person left) {
-        this.left = left;
-    }
-
-    public Person getRight() {
-        return right;
-    }
-
-    public void setRight(Person right) {
-        this.right = right;
-    }
 
     @Override
     public String toString() {
